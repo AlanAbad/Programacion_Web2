9a71a0d67011096545a54c86f5e9a2f4baa6be5a
alan <alanabad25@aragon.unam.mx>
2025-08-30 11:29:25 -0600

    Modificaciones

---
+++ 260825/app.js
@@ -0,0 +1,65 @@
+var nombre;
+    nombre = "Alan";
+    console.log(nombre);
+
+var datoString = "UNAM";
+    console.log(datoString);
+
+//
+
+let x = 4;
+let y = 6;
+
+let resultado = x + y;
+
+console.log(resultado);
+
+// concatenacion
+
+let nombreUsuario;
+let edad;
+let nacionalidad;
+let numCuenta;
+let estatura;
+
+nombreUsuario = "Alan";
+edad = 21;
+nacionalidad = "Mexicana";
+numCuenta = "320325225";
+estatura = 165;
+
+console.log(nombreUsuario);
+console.log(edad);
+console.log(nacionalidad);
+console.log(numCuenta);
+console.log(estatura);
+
+// 
+let saludo1;
+let saludo2;
+
+saludo1 = "Hola";
+saludo2 = "¿Cómo estás?";
+
+let unir = saludo1 + saludo2;
+
+console.log(unir);
+console.log(saludo1 + saludo2);
+
+//
+console.log("Mi nombre es " + nombreUsuario + " Tengo " + edad + " Soy de nacionalidad " + nacionalidad + " Mi numero de cuenta es " + numCuenta + " Mido " + estatura);
+
+// scaner
+//prompt("Ingresa dato");
+
+//let Apellido = prompt;
+//prompt("Ingresa tu nombre");
+
+//console.log(Apellido);
+
+//
+let variable1 = parseInt ( prompt("Ingresa primer número"));
+let variable2 = parseInt ( prompt("Ingresa segundo número"));
+let result = variable1 + variable2;
+
+console.log(result);
